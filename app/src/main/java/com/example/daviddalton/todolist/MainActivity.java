a332955e1e6805f272c505e2f156e850b9c26e9f
David Dalton <daviddalton@davids-mbp.lan>
2018-05-02 16:59:45 -0400

    added ability for person to see list view with items that they add

--- app/src/main/java/com/example/daviddalton/todolist/MainActivity.java
+++ app/src/main/java/com/example/daviddalton/todolist/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.daviddalton.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+
+//Think about using Room for a local database
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextItem;
+    ArrayList<String> toDoItems = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextItem = (EditText) findViewById(R.id.editText);
+
+        final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoItems);
+
+        ListView listView = findViewById(R.id.toDoList);
+        listView.setAdapter(itemsAdapter);
+
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                toDoItems.add(editTextItem.getText().toString());
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
+
+
+
+
+
 }
