9af9ee88fc566fe3169d63cb5d6aefa5836cd058
David Dalton <daviddalton@davids-mbp.lan>
2018-05-05 10:50:23 -0400

    added dialog prompt

--- app/src/main/java/com/example/daviddalton/todolist/MainActivity.java
+++ app/src/main/java/com/example/daviddalton/todolist/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.daviddalton.todolist;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoItems);
 
-        ListView listView = findViewById(R.id.toDoList);
+        final ListView listView = findViewById(R.id.toDoList);
         listView.setAdapter(itemsAdapter);
 
         Button addButton = findViewById(R.id.addButton);
@@ -35,6 +41,37 @@ public class MainActivity extends AppCompatActivity {
 
                 toDoItems.add(editTextItem.getText().toString());
                 itemsAdapter.notifyDataSetChanged();
+                editTextItem.setText("");
+                Toast.makeText(getApplicationContext(),"Item added!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(final AdapterView<?> parent, final View view, final int position, long id) {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Remove Item")
+                                .setPositiveButton("Remove", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        final String item = (String) parent.getItemAtPosition(position);
+                                        view.animate().setDuration(1000).alpha(0).withEndAction(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                toDoItems.remove(item);
+                                                itemsAdapter.notifyDataSetChanged();
+                                                view.setAlpha(1);
+                                            }
+                                        });
+                                    }
+                                })
+                                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        //Toast.makeText(getApplicationContext(),"Cancelled ", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
             }
         });
 
